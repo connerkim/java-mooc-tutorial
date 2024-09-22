8462a1e8cc82359b1644d3b2cb76bc95664004b0
connerkim <connergkim@gmail.com>
2024-09-22 00:54:33 -0400

    Squashed commit of the following:
    commit 17c0fb563e060337abb5e8f4dacddb23a2d76790
    Author: connerkim <connergkim@gmail.com>
    Date:   Sun Sep 22 00:52:44 2024 -0400
    
        Commit test change

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,7 @@
 public class Hello {
     public Hello() {
         System.out.println("Hello");
+        System.out.println("this is a test");
     }
     public static void main(String[] args) {
         System.out.println("Hello");
