0c6a120a495c00d2ce2a6ca06f4b450d54f936e8
connerkim <connergkim@gmail.com>
2024-09-22 00:58:23 -0400

    Commit test branch

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public Hello() {
         System.out.println("Hello");
-        System.out.println("this is a test");
     }
     public static void main(String[] args) {
         System.out.println("Hello");
